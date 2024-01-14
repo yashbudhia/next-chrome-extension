470b66026362fa9f45b65aff1967997c4ba8ce05
yashbudhia <yeshbudhia@gmail.com>
2024-01-14 15:27:54 +0530

    navbar-size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex h-16">
+      <div className="flex h-16 items-center pl-3 gap-4 w-32">
+        <div className="text-xl pl-2 font-bold ">ReFocus</div>
+        <div className="flex h-16 items-center gap-4 pl-4">
+          <div className="text-18 px-1 font-semibold">About</div>
+          <div className="text-18 px-1 font-semibold">Signin</div>
+          <div className="text-18 px-1 font-semibold">Login</div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,12 @@ const config: Config = {
         white: "#ffffff",
       },
       fontFamily: {},
+      fontSize: {
+        sm: ["14px", "20px"],
+        base: ["16px", "24px"],
+        lg: ["20px", "28px"],
+        xl: ["24px", "32px"],
+      },
     },
   },
   plugins: [],
