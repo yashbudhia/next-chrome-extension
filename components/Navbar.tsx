c65daaf3bb9c808d5b90b5538a28591212fcac70
yashbudhia <yeshbudhia@gmail.com>
2024-01-14 17:50:02 +0530

    nav-fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,23 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
+import { navVariants } from "./../utils/motion";
+import styles from "./../styles/index";
 
 export default function Navbar() {
   return (
-    <div className="flex h-16">
-      <div className="flex h-16 items-center pl-3 gap-4 w-32">
+    <motion.nav variants={navVariants} initial="hidden" whileInView="show">
+      <div className="absolute  inset-0  opacity-10 gradient-01 " />
+      <div className="flex h-16 items-center pl-3 gap-4 w-32 ">
         <div className="text-xl pl-2 font-bold ">ReFocus</div>
         <div className="flex h-16 items-center gap-4 pl-4">
           <div className="text-18 px-1 font-semibold">About</div>
-          <div className="text-18 px-1 font-semibold">Signin</div>
-          <div className="text-18 px-1 font-semibold">Login</div>
+          <div className="text-18 px-1 font-semibold">Pricing</div>
         </div>
+        <button className="pr-4 absolute right-3 font-semibold text-lg">
+          Signin
+        </button>
       </div>
-    </div>
+    </motion.nav>
   );
 }
