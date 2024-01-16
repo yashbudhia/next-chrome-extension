8c08376d78951403f7dd66e9cf2198519677a5df
yashbudhia <yeshbudhia@gmail.com>
2024-01-16 08:41:28 +0530

    landing-circle

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import React, { MouseEvent } from "react";
+import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+
 export default function LandingPage() {
+  let mouseX = useMotionValue(0);
+  let mouseY = useMotionValue(0);
+
+  console.log("render");
+  function handleMouseMove({ clientX, clientY, currentTarget }: MouseEvent) {
+    let { left, top } = currentTarget.getBoundingClientRect();
+    let xPosition = clientX - left;
+    let yPosition = clientY - top;
+
+    mouseX.set(xPosition);
+    mouseY.set(yPosition);
+  }
   return (
-    <div className="text-semibold text-9xl">
-      For Those Who are serious about their Productivity
-    </div>
+    <>
+      <motion.div
+        onMouseMove={handleMouseMove}
+        className=" absolute  inset-0 opacity-20 gradient-01 "
+        style={{
+          background: useMotionTemplate`radial-gradient(750px circle at ${mouseX}px ${mouseY}px,rgb(14 165 233),transparent 80%)`,
+        }}
+      />
+      <div className="text-semibold text-9xl">
+        For Those Who are serious about their Productivity
+      </div>
+    </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,6 @@ import styles from "./../styles/index";
 export default function Navbar() {
   return (
     <motion.nav variants={navVariants} initial="hidden" whileInView="show">
-      <div className="absolute  inset-0  opacity-10 gradient-01 " />
       <div className="flex h-16 items-center pl-3 gap-4 w-32 ">
         <div className="text-xl pl-2 font-bold ">ReFocus</div>
         <div className="flex h-16 items-center gap-4 pl-4">
