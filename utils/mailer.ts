8cf53e3c0daf201f4f1a513e769e1085bab81919
yashbudhia <yeshbudhia@gmail.com>
2024-01-26 02:15:39 +0530

    prisma-schema-updated

--- utils/mailer.ts
+++ utils/mailer.ts
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
+import bcryptjs from bcryptjs;
 
 export default async function sendEmail({ email, userId }: any) {
   try {
+    const hasedToken = await bcryptjs.hash(userId.toString(),10);
   } catch (error: any) {
     throw new Error(error.message);
   }
