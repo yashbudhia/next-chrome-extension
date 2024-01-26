d012de8060976024083ffee936555a70b2405810
yashbudhia <yeshbudhia@gmail.com>
2024-01-26 12:25:56 +0530

    email-magic-link-done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
+import { prisma } from "@/prisma";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import EmailProvider from "next-auth/providers/email";
 
 const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -11,6 +15,17 @@ const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
     }),
   ],
 };
--- utils/mailer.ts
+++ utils/mailer.ts
@@ -1,9 +1,39 @@
 import nodemailer from "nodemailer";
-import bcryptjs from bcryptjs;
+import bcryptjs from "bcryptjs";
+import { User } from "@prisma/client";
+import { prisma } from "@/prisma";
 
 export default async function sendEmail({ email, userId }: any) {
   try {
-    const hasedToken = await bcryptjs.hash(userId.toString(),10);
+    const hasedToken = await bcryptjs.hash(userId.toString(), 10);
+    const futureDate = new Date(Date.now() + 6 * 60 * 60 * 1000);
+
+    const updatedUser = await prisma.user.update({
+      where: { id: userId }, // Specify the user to update based on the unique identifier
+      data: {
+        verifyToken: hasedToken,
+        verifyTokenExpiry: futureDate,
+      },
+    });
+
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: "yeshbudhia@gmail.com",
+      to: email,
+      subject: "Verify your email",
+      html: `<p>Click <a href="${process.env.NEXTAUTH_URL}/verifyemail?token=${hasedToken}">here</a></p>`,
+    };
+
+    const mailresponse = await transport.sendMail(mailOptions);
+    return mailresponse;
   } catch (error: any) {
     throw new Error(error.message);
   }
